599fc321b4e9a6b83bee45154ccc82c150608fd7
Victor Onah <onahvictorc@gmail.com>
2022-08-09 13:22:23 +0100

    added form message

---
+++ internal/forms/errors.go
@@ -0,0 +1,19 @@
+package forms
+
+//declared a package level type that is not exported
+type errors map[string][]string
+
+//Adds error message for a giving form fild
+func (e errors) Add(field, message string) {
+	e[field] = append(e[field], message)
+}
+
+//Get returns the first error message
+func (e errors) Get(field string) string {
+	es := e[field]
+	if len(es) == 0 {
+		return ""
+	}
+
+	return es[0]
+}  
---
+++ internal/models/models.go
@@ -0,0 +1,9 @@
+package models
+
+//Reservation holds
+type Reservation struct {
+	FirstName string
+	LastName  string
+	Email     string
+	Phone     string
+}
